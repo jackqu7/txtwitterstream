62b523b154df57d77ba8b27e68672a77ae0d3ce6
Wade Simmons <wsimmons@gmail.com>
2010-03-17 12:16:21 -0600

    rename twitterstream to txtwitterstream

--- txtwitterstream/__init__.py
+++ txtwitterstream/__init__.py
@@ -1,3 +1,5 @@
 from twitterstream.client import Client, TweetReceiver
 
+__author__ = "Wade Simmons"
+__version__ = "0.1.0"
 __all__ = ["Client", "TweetReceiver"]
--- txtwitterstream/client.py
+++ txtwitterstream/client.py
@@ -14,9 +14,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-__author__ = "Alexandre Fiori, Wade Simmons"
-__version__ = "0.1.0"
-
 """Twisted client library for the Twitter Streaming API:
 http://apiwiki.twitter.com/Streaming-API-Documentation"""
 
