32144e2df75446aa44be9ad134d18124ac325fdc
Wade Simmons <wsimmons@gmail.com>
2010-03-17 12:28:36 -0600

    add author email address

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     version="0.0.2",
     description="A fork of Alexandre Fiori's TwistedTwitterStream Twitter client library for the Twitter Streaming API",
     author="Wade Simmons",
+    author_email="wade@wades.im",
     url="http://github.com/wadey/txtwitterstream",
     py_modules=["txtwitterstream"],
     install_requires=["Twisted >= 9.0.0"],
