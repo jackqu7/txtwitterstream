37cc18fe083e6fad4da3f5f70b216f53b6321388
Zooko Ofsimplegeo <zooko@simplegeo.com>
2010-04-21 16:41:14 -0600

    Explain why we are stopping if it was because of pyflakes warnings, and add -tt to reject tabs.

--- test.sh
+++ test.sh
@@ -10,14 +10,19 @@ git log | head -1 > .coverage-results/version-stamp.txt
 python -c 'import pkg_resources;pkg_resources.require("trialcoverage>=0.3.5")' &&
 python -c 'import pkg_resources;pkg_resources.require("coverage>=3.3.2a1z8")' &&
 
-python setup.py flakes || exit 
+python -tt setup.py flakes
+RETVAL=$?
+if [ ${RETVAL} != 0 ] ; then
+    echo "FAILED: pyflakes reported warnings -- exiting"
+    exit ${RETVAL}
+fi
 
 # Now run the tests once just to make sure all test_requires dependencies are 
 # installed before we start paying attention to code coverage.
 
 python setup.py trial > /dev/null 2> /dev/null
 
-python setup.py trial --reporter=bwverbose-coverage --rterrors
+python -tt setup.py trial --reporter=bwverbose-coverage --rterrors
 RETVAL=$?
 echo "To see coverage details run 'coverage report' or open htmlcov/index.html."
 coverage html
