5fd836c4a76fecd4af659bc4df2670f03121b5f0
Ian Eure <ian.eure@gmail.com>
2010-06-18 23:48:13 +0000

    Move setup_requires stuff to tests_require; setup doesn't require them.

--- setup.py
+++ setup.py
@@ -29,8 +29,7 @@ setup(
     packages = find_packages(),
     test_suite="txtwitterstream.test",
     install_requires=['Twisted >= 9.0.0', 'pyutil >= 1.7.0'],
-    setup_requires=['setuptools_trial', 'setuptools_pyflakes'],
-    tests_require=['mock'],
+    tests_require=['setuptools_trial', 'setuptools_pyflakes', 'mock'],
     license = "BSD",
     classifiers=trove_classifiers,
     zip_safe = False, # We prefer unzipped for easier access.
